409a726ba59a25b46823112f86bbbaf212d97aba
Rahman <rahmanrejepov777@gmail.com>
2022-08-19 13:37:33 +0300

    Added test classes and fixed some bugs

--- src/test/java/controllerTests/paymentControllerTest.java
+++ src/test/java/controllerTests/paymentControllerTest.java
@@ -6,7 +6,6 @@ import account.Domain.PaymentsToEmployee;
 import account.Domain.User;
 import account.Repositories.PaymentRepo;
 import account.Repositories.UserRepo;
-import account.Security.EmployeeAuthentication.EmployeePostOrGetAuthentication;
 import org.json.JSONArray;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -14,13 +13,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -46,7 +41,6 @@ public class paymentControllerTest {
 
     @InjectMocks
     private PaymentController paymentController;
-
 
     private MockMvc mockMvc;
 
